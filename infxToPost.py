df214d1088ad1c678e511dfa0b7097742e158e7d
Guilherme Portella <fguilhermepll@gmail.com>
2021-09-01 10:21:47 -0300

    Update infxToPost.py

--- infxToPost.py
+++ infxToPost.py
@@ -1,21 +1,22 @@
 class Stack:
-     def __init__(self):
-         self.items = []
+    items = None
+    def __init__(self):
+        self.items = []
 
-     def isEmpty(self):
-         return self.items == []
+    def isEmpty(self):
+        return self.items == []
 
-     def push(self, item):
-         self.items.append(item)
+    def push(self, item):
+        self.items.append(item)
 
-     def pop(self):
-         return self.items.pop()
+    def pop(self):
+        return self.items.pop()
 
-     def peek(self):
-         return self.items[len(self.items)-1]
+    def peek(self):
+        return self.items[len(self.items)-1]
 
-     def size(self):
-         return len(self.items)
+    def size(self):
+        return len(self.items)
 
 def precedence(c):
     if(c == '+' or c == '-'):
@@ -26,17 +27,23 @@ def precedence(c):
         return 3
     else:
         return 0
-        
+def split(word):
+    return [char for char in word]
+    
 def infixToPost(exp):
     stack = Stack()
     stack.push('#')
     output = []
-    for c in exp.split():
-        if (c.isnumeric() or c.isalpha()):
+
+    splitExp = split(exp)
+    #print(splitExp)
+    
+    for c in splitExp:
+        if(c == ' '):
+            pass
+        elif (c.isnumeric() or c.isalpha()):
             output.append(c)
         elif (c == '('):
-            stack.push(c)
-        elif (c == '^'):
             stack.push(c)
         elif (c == ')'):
             while (not stack.isEmpty() and stack.peek() != '('):
@@ -44,18 +51,15 @@ def infixToPost(exp):
                 output.append(item)
             stack.pop()
         else:
-            if(precedence(c) > precedence(stack.peek())):
-                stack.push(c)
-            else:
-                while (not stack.isEmpty() and precedence(c) <= precedence(stack.peek())):
-                    item = stack.pop()
-                    output.append(item)
+            while (not stack.isEmpty() and precedence(c) <= precedence(stack.peek())):
+                output.append(stack.pop())
 
-                output.append(c)
+            stack.push(c)
     while (stack.peek() != '#'):
         output.append(stack.peek())
         stack.pop()
     outputJoin = ''.join(output)
     return outputJoin
-print(infixToPost('x ^ y / ( 5 * z ) + 2'))
-print(infixToPost('3 + 1'))
+    
+print(infixToPost('3+1'))
+print(infixToPost('x^y/(5*z)+10'))
